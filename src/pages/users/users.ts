8ffb9d3db290e76ed96825d64e0a38251f6f9321
Jason Stollman <jason.stollman@gmail.com>
2018-09-25 15:35:38 -0500

    Added Loader

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController,
+ } from 'ionic-angular';
 
 import { UsersProvider } from '../../providers/users/users';
 import { User } from '../../models/user/user';
@@ -23,7 +28,8 @@ export class UsersPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private usersProvider: UsersProvider
+    private usersProvider: UsersProvider,
+    private loadingCtrl: LoadingController,
     ){
   }
 
@@ -32,11 +38,22 @@ export class UsersPage {
   }
 
   private getUsers(): void{
+
+    let loader = this.loadingCtrl.create({
+      content: 'loading...',
+    });
+
+    loader.present();
+
     this.usersProvider.getUsers().subscribe(
+
       (response:any)=>{
         this.users = response.users;
+        loader.dismiss();
       }
     );
   }
+
+
 
 }
